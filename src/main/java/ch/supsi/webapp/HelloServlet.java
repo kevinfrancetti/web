e1142bb96d7765c46adf092a5c8d9c907cb2ba92
kevinfrancetti <kevin.francetti91@gmail.com>
2019-10-07 15:15:51 +0200

    nothing special

--- src/main/java/ch/supsi/webapp/HelloServlet.java
+++ src/main/java/ch/supsi/webapp/HelloServlet.java
@@ -12,12 +12,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
-*Qua siamo sul branch dev
-*
- */
-
-
 @WebServlet(value="/hello")
 @SuppressWarnings("serial")
 public class HelloServlet extends HttpServlet {
