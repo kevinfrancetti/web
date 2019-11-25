9c572a186f41f79294a2aaba4db5df6e1b8ed562
kevinfrancetti <kevin.francetti91@gmail.com>
2019-11-25 21:09:09 +0100

    work in progress

--- src/main/java/ch/supsi/web/controller/ControllerItem.java
+++ src/main/java/ch/supsi/web/controller/ControllerItem.java
@@ -8,14 +8,17 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 @RestController
 public class ControllerItem {
 
-    static List<Item> itemList = new ArrayList<>();
+    List<Item> itemList = new ArrayList<>();
+    static Random rnd = new Random();
 
     static {
-        itemList.add(new Item(1,"kev", "bro", "loco"));
+        rnd.setSeed(System.currentTimeMillis());
     }
 
     @RequestMapping(value="/items", method = RequestMethod.GET)
@@ -23,26 +26,43 @@ public class ControllerItem {
         return new ResponseEntity<>(itemList, HttpStatus.OK);
     }
 
+    private Optional<Item> getItem(int id){
+        return itemList.stream().filter(item -> item.getId() == id).findFirst();
+    }
+
+
     @RequestMapping(value="/items/{id}", method = RequestMethod.GET)
-    ResponseEntity<Item> getItem(@PathVariable int id){
-        getItem(id);
-        return new ResponseEntity<>(itemList.get(id), HttpStatus.OK);
+    ResponseEntity<Item> get(@PathVariable int id){
+        Optional<Item> itemAsked = getItem(id);
+        return itemAsked.isPresent() ? new ResponseEntity<>(itemAsked.get(), HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @RequestMapping(value="/items", method = RequestMethod.POST)
     public ResponseEntity<Item> post(@RequestBody Item item){
+        item.setId(rnd.nextInt());
         itemList.add(item);
         return new ResponseEntity<>(item, HttpStatus.CREATED);
     }
 
-    @RequestMapping(value="/items", method = RequestMethod.PUT)
-    String test3(){
-        return "Hello worldT";
+    @RequestMapping(value="/items/{id}", method = RequestMethod.PUT)
+    ResponseEntity<Item> put(@PathVariable int id, @RequestBody Item item){
+        Optional<Item> itemAskedToChange = getItem(id);
+        if(!itemAskedToChange.isPresent()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        Item oldItem = itemAskedToChange.get();
+        itemList.remove(oldItem);
+        item.setId(id);
+        itemList.add(item);
+        return new ResponseEntity<>(oldItem, HttpStatus.OK);
     }
 
     @RequestMapping(value="/items/{id}", method = RequestMethod.DELETE)
-    String delete(@PathVariable int id){
-        itemList.remove(id);
-        return "Hello world4";
+    ResponseEntity<Item> delete(@PathVariable int id){
+        Optional<Item> itemAskedToDelete = getItem(id);
+
+        if(itemAskedToDelete.isPresent()){
+            Item removedItem = itemAskedToDelete.get();
+            itemList.remove(removedItem);
+            return new ResponseEntity<>(removedItem, HttpStatus.OK);
+        }else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
